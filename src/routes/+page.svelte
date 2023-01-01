6c58271570009594912f859aa103689ad9f616e3
sadrishehu <sadrishehu@gmail.com>
2023-01-01 14:10:34 +0100

    GB-002: add nav menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <TabBar tabs={['Home', 'Merchandise', 'About Us']} let:tab bind:active>
+        <Tab {tab} href="#{tab.replace(/ /g, '_').toLowerCase()}">
+            <Label>{tab}</Label>
+        </Tab>
+    </TabBar>
+</div>
+  
+<script lang="ts">
+    import Tab, { Label } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
+
+    let active = 'Home';
+</script>
